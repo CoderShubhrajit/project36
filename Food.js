31b41c6c7c29d0d4f3919bef395d929c8ec21861
CoderShubhrajit <74853715+CoderShubhrajit@users.noreply.github.com>
2021-02-15 11:33:08 +0530

    Add files via upload
    .......................

---
+++ Food.js
@@ -0,0 +1,45 @@
+class Food
+{
+    constructor()
+    {
+        this.image = loadImage("milkImage.png");
+        this.foodStock = 15;
+    }
+    getFoodStock()
+    {
+        database.ref("lunch/food");
+    }
+    updateFoodStock(foodStock)
+    {
+        foodStock = this.foodStock,
+        foodStock = foodStock + 1;
+    }
+    deductFood()
+    {
+        if(this.foodStock>0){
+            this.foodStock = this.foodStock - 1;
+        }
+    }
+    display()
+    {
+        var x = 80;
+        var y = 100;
+
+        imageMode(CENTER);
+        //image(this.image,720,220,70,70);
+
+        if (this.foodStock!=0)
+        {
+            for(var i = 0;i<this.foodStock;i++)
+            {
+                if (i%10===0)
+                {
+                    x = 80;
+                    y = y+70;
+                }
+                image(this.image,x,y,50,50);
+                x = x+30;
+            }
+        }
+    }
+}
